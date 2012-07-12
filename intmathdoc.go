ff22732902496ec7751da3a55be276a906df1a14
J L vanderZwan <J.L.vanderZwan@gmail.com>
2012-07-12 08:01:20 +0200

    Added Min/Max functions

--- intmathdoc.go
+++ intmathdoc.go
@@ -1,4 +1,9 @@
 // intmath is a dummy package - the real library is in the subpackages.
 // Import the relevant type to use math functions tailored to that type.
 // 
+// Currently implemented:
+// Abs(x) (only for signed types, obviously)
+// Min(x, y)
+// Max(x, y)
+// Pow(x, y)
 package intmath
