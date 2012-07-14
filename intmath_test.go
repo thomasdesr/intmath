d52b248c7d4aa93d006dead43fa6db553e79db9e
J L vanderZwan <J.L.vanderZwan@gmail.com>
2012-07-14 19:32:08 +0200

    changed Log2 algorithm again. i64/u64 2.5x faster, i32/u32 2x

--- intmath_test.go
+++ intmath_test.go
@@ -1,15 +1,19 @@
 package intmath
 
 import (
-	"intmath/i32"
-	"intmath/i64"
-	"intmath/intgr"
-	"intmath/u32"
-	"intmath/u64"
-	"intmath/uintgr"
+	"code.google.com/p/intmath/i32"
+	"code.google.com/p/intmath/i64"
+	"code.google.com/p/intmath/intgr"
+	"code.google.com/p/intmath/u32"
+	"code.google.com/p/intmath/u64"
+	"code.google.com/p/intmath/uintgr"
 	"math"
 	"testing"
 )
+
+// ================================================
+// ==== Benchmarking different Abs algorithms ====
+// ================================================
 
 // Bitmasking Abs as I found it in package bitbucket.org/SyntaxK/imath. int32 version
 func BenchmarkAbs32bit1(b *testing.B) {
@@ -42,6 +46,10 @@ func BenchmarkAbs64bit2(b *testing.B) {
 	}
 }
 
+// ================================================
+// ==== Benchmarking different Epx algorithms ====
+// ================================================
+
 // Using i64 package's simple exp function
 func BenchmarkExpInt64(b *testing.B) {
 	for i := 0; i < b.N; i++ {
@@ -60,7 +68,7 @@ func BenchmarkExpFloat64ToInt64(b *testing.B) {
 	}
 }
 
-// Using i64 package's simple exp function
+// Using i32 package's simple exp function
 func BenchmarkExpInt32(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for j := int32(0); j < 15; j++ {
@@ -79,55 +87,238 @@ func BenchmarkExpFloat64ToInt32(b *testing.B) {
 	}
 }
 
+// ================================================
+// ==== Benchmarking different Log2 algorithms ====
+// ================================================
+
+func BenchmarkLog2NaiveU64(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		for i := uint64(1); i < 64; i++ {
+			_ = log2U64(1 << i)
+		}
+
+	}
+}
+
+func BenchmarkLog2U64(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		for i := uint64(1); i < 64; i++ {
+			_ = loopU64Log2(1 << i)
+		}
+	}
+}
+
+func BenchmarkLog2bitmaskU64(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		for i := uint64(1); i < 64; i++ {
+			_ = bitmaskU64Log2(1 << i)
+		}
+	}
+}
+
+func log2U64(n uint64) (r uint64) {
+	for n > 1 {
+		n >>= 1
+		r++
+	}
+	return
+}
+
+func loopU64Log2(n uint64) uint64 {
+	for i := uint64(56); i >= 0; i -= 8 {
+		if n>>i > 0 {
+			for ; ; i++ {
+				if n>>i == 1 {
+					return i
+				}
+			}
+		}
+	}
+	return 0
+}
+
+func bitmaskU64Log2(n uint64) uint64 {
+	// uint is always set to the native word width, and it is therefore 
+	// logical to expect that the operation is significantly faster on 
+	// 32bit systems if we use it instead of uint64. On a netbook with
+	// an Atom N280 CPU the benchmark confirm that this speeds up the
+	// algorithm by 25%. The unnecessary cast on 64bit systems was 
+	// considered worth the trade-off.
+	var r, v uint
+	if n >= 1<<32 {
+		r += 32
+		v = uint(n >> 32)
+	} else {
+		v = uint(n)
+	}
+	if v >= 1<<16 {
+		r += 16
+		v >>= 16
+	}
+	if v >= 1<<8 {
+		r += 8
+		v >>= 8
+	}
+	if v >= 1<<4 {
+		r += 4
+		v >>= 4
+	}
+	if v >= 1<<2 {
+		r += 2
+		v >>= 2
+	}
+	r += v >> 1
+	return uint64(r)
+}
+
+func BenchmarkLog2NaiveU32(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		for i := uint32(1); i < 32; i++ {
+			_ = log2U32(1 << i)
+		}
+
+	}
+}
+
+func BenchmarkLog2U32(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		for i := uint32(1); i < 32; i++ {
+			_ = loopU32Log2(1 << i)
+		}
+	}
+}
+
+func BenchmarkLog2bitmaskU32(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		for i := uint32(1); i < 32; i++ {
+			_ = bitmaskU32Log2(1 << i)
+		}
+	}
+}
+
+func log2U32(n uint32) (r uint32) {
+	for n > 1 {
+		n >>= 1
+		r++
+	}
+	return
+}
+
+func loopU32Log2(n uint32) uint32 {
+	for i := uint32(25); i >= 0; i -= 8 {
+		if n>>i > 0 {
+			for ; ; i++ {
+				if n>>i == 1 {
+					return i
+				}
+			}
+		}
+	}
+	return 0
+}
+
+func bitmaskU32Log2(n uint32) (r uint32) {
+	if n >= 1<<16 {
+		r += 16
+		n >>= 16
+	}
+	if n >= 1<<8 {
+		r += 8
+		n >>= 8
+	}
+	if n >= 1<<4 {
+		r += 4
+		n >>= 4
+	}
+	if n >= 1<<2 {
+		r += 2
+		n >>= 2
+	}
+	r += n >> 1
+	return
+}
+
+// ====================================================
+// ==== Testing Log2 algorithm used in the library ====
+// ====================================================
+
 func TestLog2(t *testing.T) {
-	fmt.Println("Testing Log2")
-	iMax := 1
+	t.Log("Testing Log2\n")
+	iMax := uint(1)
 	if iMax<<32 == 0 {
 		iMax = 31
 	} else {
 		iMax = 63
 	}
-	for i := 0; i < iMax; i++ {
-		if intgr.Log2(1<<uint(i)) != i {
-			t.Logf("intgr.Log2(1<<%v) == %v\n", i, intgr.Log2(1<<uint(i)))
-			t.FailNow()
+	for i := uint(0); i < iMax; i++ {
+		for j := uint(0); j < i; j++ {
+			if intgr.Log2((1<<i)+(1<<j)) != int(i) {
+				t.Logf("intgr.Log2(%v) == %v\n", (1<<i)+(1<<j), intgr.Log2((1<<i)+(1<<j)))
+				t.FailNow()
+			}
+		}
+	}
+	if intgr.Log2(-1) != -1 {
+		t.Logf("intgr.Log2(-1) == %v\n", intgr.Log2(-1))
+		t.FailNow()
+	}
+
+	for i := uint(0); i < iMax; i++ {
+		for j := uint(0); j < i; j++ {
+			if uintgr.Log2((1<<i)+(1<<j)) != i {
+				t.Logf("intgr.Log2(%v) == %v\n", (1<<i)+(1<<j), uintgr.Log2((1<<i)+(1<<j)))
+				t.Fail()
+			}
 		}
 	}
 
-	for i := uint(0); i < uint(iMax); i++ {
-		if uintgr.Log2(1<<i) != i {
-			t.Logf("uintgr.Log2(1<<%v) == %v\n", i, uintgr.Log2(1<<uint(i)))
-			t.Fail()
+	for i := uint32(0); i < uint32(31); i++ {
+		for j := uint32(0); j < i; j++ {
+			if i32.Log2((1<<i)+(1<<j)) != int32(i) {
+				t.Logf("i32.Log2(%v) == %v\n", (1<<i)+(1<<j), i32.Log2((1<<i)+(1<<j)))
+				t.FailNow()
+			}
 		}
 	}
-	for i := int32(0); i < 31; i++ {
-		if i32.Log2(1<<uint(i)) != i {
-			t.Logf("i32.Log2(1<<%v) == %v\n", i, i32.Log2(1<<uint(i)))
-			t.Fail()
+	if i32.Log2(-1) != int32(-1) {
+		t.Logf("i32.Log2(-1) == %v\n", i32.Log2(-1))
+		t.FailNow()
+	}
+
+	for i := uint32(0); i < uint32(31); i++ {
+		for j := uint32(0); j < i; j++ {
+			if u32.Log2((1<<i)+(1<<j)) != i {
+				t.Logf("u32.Log2(%v) == %v\n", (1<<i)+(1<<j), u32.Log2((1<<i)+(1<<j)))
+				t.FailNow()
+			}
 		}
 	}
-	for i := int64(0); i < 63; i++ {
-		if i64.Log2(1<<uint(i)) != i {
-			t.Logf("i64.Log2(1<<%v) == %v\n", i, i64.Log2(1<<uint(i)))
-			t.Fail()
+
+	for i := uint64(0); i < uint64(63); i++ {
+		for j := uint64(0); j < i; j++ {
+			if i64.Log2((1<<i)+(1<<j)) != int64(i) {
+				t.Logf("i64.Log2(%v) == %v\n", (1<<i)+(1<<j), i64.Log2((1<<i)+(1<<j)))
+				t.FailNow()
+			}
 		}
 	}
-	for i := uint32(0); i < 32; i++ {
-		if u32.Log2(1<<uint(i)) != i {
-			t.Logf("u32.Log2(1<<%v) == %v\n", i, u32.Log2(1<<uint(i)))
-			t.Fail()
-		}
+	if i64.Log2(-1) != int64(-1) {
+		t.Logf("i64.Log2(-1) == %v\n", i64.Log2(-1))
+		t.FailNow()
 	}
-	for i := uint64(0); i < 64; i++ {
-		if u64.Log2(1<<uint(i)) != i {
-			t.Logf("u64.Log2(1<<%v) != %v\n", i, u64.Log2(1<<uint(i)))
-			t.Fail()
+
+	for i := uint64(0); i < uint64(63); i++ {
+		for j := uint64(0); j < i; j++ {
+			if u64.Log2((1<<i)+(1<<j)) != i {
+				t.Logf("u64.Log2(%v) == %v\n", (1<<i)+(1<<j), u64.Log2((1<<i)+(1<<j)))
+				t.FailNow()
+			}
 		}
 	}
 }
 
 func TestSqrt(t *testing.T) {
-	fmt.Println("Testing Sqrt")
+	t.Logf("Testing Sqrt\n")
 	i32Max := int32(0x7FFF)
 	i64Max := int64(0x7FFFFFFF)
 	u32Max := uint32(0xFFFF)
--- u64/log2.go
+++ u64/log2.go
@@ -3,14 +3,31 @@ package u64
 // Log2 returns log base 2 of n. It's the same as index of the highest
 // bit set in n. n == 0 returns 0
 func Log2(n uint64) uint64 {
-	for i := uint64(56); i >= 0; i -= 8 {
-		if n>>i > 0 {
-			for ; ; i++ {
-				if n>>i == 1 {
-					return i
-				}
-			}
-		}
+	// Using uint instead of uint64 is about 25% faster
+	// on x86 systems with the default Go compiler.
+	var r, v uint
+	if n >= 1<<32 {
+		r += 32
+		v = uint(n >> 32)
+	} else {
+		v = uint(n)
 	}
-	return 0
+	if v >= 1<<16 {
+		r += 16
+		v >>= 16
+	}
+	if v >= 1<<8 {
+		r += 8
+		v >>= 8
+	}
+	if v >= 1<<4 {
+		r += 4
+		v >>= 4
+	}
+	if v >= 1<<2 {
+		r += 2
+		v >>= 2
+	}
+	r += v >> 1
+	return uint64(r)
 }
